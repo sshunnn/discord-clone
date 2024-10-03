543a9a1539568472c96624364f539325c75f8b30
shunprog <shun.prog@gmail.com>
2024-10-03 21:26:29 +0900

    add google auth

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,47 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { useSelector } from "react-redux";
 import "./App.scss";
+import { useAppDispatch, useAppSelector } from "./app/hooks";
 import Sidebar from "./components/sidebar/Sidebar";
 import Chat from "./components/chat/Chat";
+import Login from "./components/login/Login";
+import { auth } from "./firebase";
+import { login, logout } from "./features/userSlice";
 
 function App() {
+  const user = useAppSelector((state) => state.user);
+  const dispatch = useAppDispatch();
+
+  useEffect(() => {
+    auth.onAuthStateChanged((loginUser) => {
+      // console.log("loginUser :>> ", loginUser);
+      if (loginUser) {
+        dispatch(
+          login({
+            uid: loginUser.uid,
+            photoUrl: loginUser.photoURL,
+            email: loginUser.email,
+            displayName: loginUser.displayName,
+          })
+        );
+      } else {
+        dispatch(logout());
+      }
+    });
+  }, [dispatch]);
+
   return (
     <div className="App">
-      {/* sidebar */}
-      <Sidebar />
-
-      {/* chat */}
-      <Chat />
+      {user ? (
+        <>
+          <Sidebar />
+          <Chat />
+        </>
+      ) : (
+        <>
+          <Login />
+        </>
+      )}
     </div>
   );
 }
---
+++ src/components/login/Login.tsx
@@ -0,0 +1,27 @@
+import React from "react";
+import "./Login.scss";
+import { Button } from "@mui/material";
+import { signInWithPopup } from "firebase/auth";
+import { auth, provider } from "../../firebase";
+
+const Login = () => {
+
+    const signIn = () => {
+        // Google認証
+        signInWithPopup(auth, provider).catch((err) => {
+            console.log(err.massege);
+        });
+    };
+
+  return (
+    <div className="login">
+      <div className="loginLogo">
+        <img src="./discordIcon.png" alt="" />
+      </div>
+
+      <Button onClick={signIn}>ログイン</Button>
+    </div>
+  );
+};
+
+export default Login;
